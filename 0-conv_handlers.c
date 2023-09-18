08c24693ce491978a0e5e5341c45b747f2b951dc
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-18 09:52:31 +0300

    Update 0-conv_handlers.c

--- 0-conv_handlers.c
+++ 0-conv_handlers.c
@@ -33,7 +33,7 @@ int handle_conv_c(struct struct_conversion *conv, va_list list)
  */
 int handle_conv_s(struct struct_conversion *conv, va_list list)
 {
-	char nil[] = "(null)";
+	char nil[] = "(nill)";
 	char *new, *s;
 	int direction, width, len;
 	
