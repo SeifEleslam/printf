ab68a3de4f4c972e6fd12e51edec6a9b60af472e
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-18 04:14:26 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+struct struct_conversion
+{
+  char conv;
+  char *flags;
+  int p;
+  int width;
+} struct_conversion;
+
+typedef int (*conv_actions)(struct struct_conversion *, va_list);
+
+int _printf(const char *format, ...);
+void define_consts(conv_actions *conversions, char *flags);
+void init_conv(struct struct_conversion *conv, char *conv_flag);
+void valid_exp(char *s, conv_actions *conversions, char *flags, struct struct_conversion *conv);
+int to_int(char **s);
+
+int _putstr(char *s, unsigned int n);
+int contains(char *s, char c);
+int _strlen(char *s);
+void _strcp(char *to, char *from);
+
+int assign_width(char *new, char *s, int total_width, int width, int direction);
+int assign_width_pos(char *new, char *s, int total_width, int width);
+int assign_width_neg(char *new, char *s, int total_width, int width);
+
+int handle_conv_c(struct struct_conversion *conv, va_list list);
+int handle_conv_s(struct struct_conversion *conv, va_list list);
+
+#endif
