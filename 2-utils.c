8170df14b78a99dc890deab5c827c31320c6fa76
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-18 17:48:34 +0300

    Update 2-utils.c

--- 2-utils.c
+++ 2-utils.c
@@ -3,14 +3,30 @@
 /**
  * _intlen - print strings
  * @num: - char to sperate ints
+ * @base: - char to sperate ints
  * Return: int
  */
-int _intlen(int num)
+int _intlen(int num, int base)
 {
 	int i;
 
 	for (i = 0; num != 0; i++)
-		num /= 10;
+		num /= base;
+	return (i > 0 ? i : 1);
+}
+
+/**
+ * _uintlen - print strings
+ * @num: - char to sperate ints
+ * @base: - char to sperate ints
+ * Return: int
+ */
+int _uintlen(unsigned int num, unsigned int base)
+{
+	int i;
+	
+	for (i = 0; num != 0; i++)
+		num /= base;
 	return (i > 0 ? i : 1);
 }
 
@@ -33,5 +49,25 @@ void int_to_str(int num, char *new_num, int len, int sign)
 	{
 		new_num[i] = (num % 10) + '0';
 		num /= 10;
+	}
+}
+
+/**
+ * uint_to_str - print strings
+ * @num: - char to sperate ints
+ * @new_num: - char to sperate ints
+ * @len: - char to sperate ints
+ * @base: - char to sperate ints
+ */
+void uint_to_str(unsigned int num, char *new_num, int len, unsigned int base)
+{
+	int i;
+	
+	if (num < 0)
+	num *= -1;
+	for (i = len - 1; i >= 0; i--)
+	{
+		new_num[i] = (num % base) + '0';
+		num /= base;
 	}
 }
