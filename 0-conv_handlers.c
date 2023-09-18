5a4aff652fc8436ab05db3984acecff7e8cd74c8
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-18 14:39:25 +0300

    Update 0-conv_handlers.c

--- 0-conv_handlers.c
+++ 0-conv_handlers.c
@@ -64,3 +64,45 @@ int handle_conv_percent(__attribute__((unused)) struct struct_conversion *conv, 
 	char percent = '%';
 	return _putstr(&percent, 1);
 }
+
+
+/**
+ * handle_conv_i - print strings
+ * @conv: - char to sperate ints
+ * @list: - char to sperate ints
+ * Return: int
+ */
+int handle_conv_i(struct struct_conversion *conv, va_list list)
+{
+	char *new_all, *new_num;
+	int direction, width, len, num, sign;
+	
+	sign = 0;
+	direction = 1;
+	num = (int)va_arg(list, int);
+	len = _intlen(num);
+	if (num < 0 || contains(conv->flags, '+'))
+	{
+		len++;
+		sign = 1;
+	}
+	if (contains(conv->flags, '-'))
+		direction = -1;
+	if (contains(conv->flags, '.') && conv->p >= len)
+		len = conv->p + sign;
+	new_num = malloc(sizeof(char) * len);
+	if (!new_num)
+		exit(1);
+	int_to_str(num, new_num, len, sign);
+	width = conv->width > len ? conv->width : len;
+	new_all = malloc(sizeof(char) * width);
+	if (!new_all)
+	{
+		free(new_num);
+		exit(1);
+	}
+	len = assign_width(new_all, new_num, width, len, direction);
+	free(new_num);
+	free(new_all);
+	return len;
+}
