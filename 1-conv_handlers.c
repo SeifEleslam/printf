43524fd1a217414ff5f344eca769a523590e4163
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-19 04:13:49 +0300

    Update 1-conv_handlers.c

--- 1-conv_handlers.c
+++ 1-conv_handlers.c
@@ -78,3 +78,44 @@ int handle_conv_o(struct struct_conversion *conv, va_list list)
     free(new_all);
     return len;
 }
+
+
+/**
+ * handle_conv_x - prints an int
+ * @conv: va_list
+ * @list: va_list
+ * Return: int
+ */
+int handle_conv_x(struct struct_conversion *conv, va_list list)
+{
+    char *new_all, *new_num;
+    int direction, width, len;
+    unsigned int num;
+
+    direction = 1;
+    num = (unsigned int)va_arg(list, unsigned int);
+    len = _uintlen(num, 16);
+    if (contains(conv->flags, '-'))
+        direction = -1;
+    if (contains(conv->flags, '.') && conv->p > len)
+        len = conv->p;
+    if (contains(conv->flags, '#') && num > 0)
+        len += 2;
+    new_num = malloc(sizeof(char) * len);
+    if (!new_num)
+        exit(1);
+    uint_to_str(num, new_num, len, 16, conv->conv == 'X' ? 1 : 0);
+    if (contains(conv->flags, '#') && num > 0)
+        new_num[0] = '0', new_num[1] = 'x';
+    width = conv->width > len ? conv->width : len;
+    new_all = malloc(sizeof(char) * width);
+    if (!new_all)
+    {
+        free(new_num);
+        exit(1);
+    }
+    len = assign_width(new_all, new_num, width, len, direction);
+    free(new_num);
+    free(new_all);
+    return len;
+}
