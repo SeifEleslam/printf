413bb3ede735443efe5ec039ee4cd547ccb13b64
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-09-19 10:17:44 +0300

    Update 1-conv_handlers.c

--- 1-conv_handlers.c
+++ 1-conv_handlers.c
@@ -8,34 +8,34 @@
  */
 int handle_conv_u(struct struct_conversion *conv, va_list list)
 {
-    char *new_all, *new_num;
-    int direction, width, len;
-    unsigned int num;
-
-    direction = 1;
-    num = (unsigned int)va_arg(list, unsigned int);
-    len = _uintlen(num, 10);
-    if (contains(conv->flags, '-'))
-        direction = -1;
-    if (contains(conv->flags, '.'))
-        len++;
-    if (contains(conv->flags, '.') && conv->p > len)
-        len = conv->p;
-    new_num = malloc(sizeof(char) * len);
-    if (!new_num)
-        exit(1);
-    uint_to_str(num, new_num, len, 10, 0);
-    width = conv->width > len ? conv->width : len;
-    new_all = malloc(sizeof(char) * width);
-    if (!new_all)
-    {
-        free(new_num);
-        exit(1);
-    }
-    len = assign_width(new_all, new_num, width, len, direction);
-    free(new_num);
-    free(new_all);
-    return len;
+	char *new_all, *new_num;
+	int direction, width, len;
+	unsigned int num;
+	
+	direction = 1;
+	num = (unsigned int)va_arg(list, unsigned int);
+	len = _uintlen(num, 10);
+	if (contains(conv->flags, '-'))
+		direction = -1;
+	if (contains(conv->flags, '.'))
+		len++;
+	if (contains(conv->flags, '.') && conv->p > len)
+		len = conv->p;
+	new_num = malloc(sizeof(char) * len);
+	if (!new_num)
+		exit(1);
+	uint_to_str(num, new_num, len, 10, 0);
+	width = conv->width > len ? conv->width : len;
+	new_all = malloc(sizeof(char) * width);
+	if (!new_all)
+	{
+		free(new_num);
+		exit(1);
+	}
+	len = assign_width(new_all, new_num, width, len, direction);
+	free(new_num);
+	free(new_all);
+	return len;
 }
 
 
@@ -47,36 +47,36 @@ int handle_conv_u(struct struct_conversion *conv, va_list list)
  */
 int handle_conv_o(struct struct_conversion *conv, va_list list)
 {
-    char *new_all, *new_num;
-    int direction, width, len;
-    unsigned int num;
-
-    direction = 1;
-    num = (unsigned int)va_arg(list, unsigned int);
-    len = _uintlen(num, 8);
-    if (contains(conv->flags, '-'))
-        direction = -1;
-    if (contains(conv->flags, '#') && num > 0)
-        len++;
-    if (contains(conv->flags, '.') && conv->p > len)
-        len = conv->p;
-    new_num = malloc(sizeof(char) * len);
-    if (!new_num)
-        exit(1);
-    uint_to_str(num, new_num, len, 8, 0);
-    if (contains(conv->flags, '#') && num > 0)
-        new_num[0] = '0';
-    width = conv->width > len ? conv->width : len;
-    new_all = malloc(sizeof(char) * width);
-    if (!new_all)
-    {
-        free(new_num);
-        exit(1);
-    }
-    len = assign_width(new_all, new_num, width, len, direction);
-    free(new_num);
-    free(new_all);
-    return len;
+	char *new_all, *new_num;
+	int direction, width, len;
+	unsigned int num;
+	
+	direction = 1;
+	num = (unsigned int)va_arg(list, unsigned int);
+	len = _uintlen(num, 8);
+	if (contains(conv->flags, '-'))
+		direction = -1;
+	if (contains(conv->flags, '#') && num > 0)
+		len++;
+	if (contains(conv->flags, '.') && conv->p > len)
+		len = conv->p;
+	new_num = malloc(sizeof(char) * len);
+	if (!new_num)
+		exit(1);
+	uint_to_str(num, new_num, len, 8, 0);
+	if (contains(conv->flags, '#') && num > 0)
+		new_num[0] = '0';
+	width = conv->width > len ? conv->width : len;
+	new_all = malloc(sizeof(char) * width);
+	if (!new_all)
+	{
+		free(new_num);
+		exit(1);
+	}
+	len = assign_width(new_all, new_num, width, len, direction);
+	free(new_num);
+	free(new_all);
+	return len;
 }
 
 
@@ -88,36 +88,36 @@ int handle_conv_o(struct struct_conversion *conv, va_list list)
  */
 int handle_conv_x(struct struct_conversion *conv, va_list list)
 {
-    char *new_all, *new_num;
-    int direction, width, len;
-    unsigned int num;
-
-    direction = 1;
-    num = (unsigned int)va_arg(list, unsigned int);
-    len = _uintlen(num, 16);
-    if (contains(conv->flags, '-'))
-        direction = -1;
-    if (contains(conv->flags, '.') && conv->p > len)
-        len = conv->p;
-    if (contains(conv->flags, '#') && num > 0)
-        len += 2;
-    new_num = malloc(sizeof(char) * len);
-    if (!new_num)
-        exit(1);
-    uint_to_str(num, new_num, len, 16, conv->conv == 'X' ? 1 : 0);
-    if (contains(conv->flags, '#') && num > 0)
-        new_num[0] = '0', new_num[1] = 'x';
-    width = conv->width > len ? conv->width : len;
-    new_all = malloc(sizeof(char) * width);
-    if (!new_all)
-    {
-        free(new_num);
-        exit(1);
-    }
-    len = assign_width(new_all, new_num, width, len, direction);
-    free(new_num);
-    free(new_all);
-    return len;
+	char *new_all, *new_num;
+	int direction, width, len;
+	unsigned int num;
+	
+	direction = 1;
+	num = (unsigned int)va_arg(list, unsigned int);
+	len = _uintlen(num, 16);
+	if (contains(conv->flags, '-'))
+		direction = -1;
+	if (contains(conv->flags, '.') && conv->p > len)
+		len = conv->p;
+	if (contains(conv->flags, '#') && num > 0)
+		len += 2;
+	new_num = malloc(sizeof(char) * len);
+	if (!new_num)
+		exit(1);
+	uint_to_str(num, new_num, len, 16, conv->conv == 'X' ? 1 : 0);
+	if (contains(conv->flags, '#') && num > 0)
+		new_num[0] = '0', new_num[1] = 'x';
+	width = conv->width > len ? conv->width : len;
+	new_all = malloc(sizeof(char) * width);
+	if (!new_all)
+	{
+		free(new_num);
+		exit(1);
+	}
+	len = assign_width(new_all, new_num, width, len, direction);
+	free(new_num);
+	free(new_all);
+	return len;
 }
 
 /**
@@ -128,35 +128,39 @@ int handle_conv_x(struct struct_conversion *conv, va_list list)
  */
 int handle_conv_S(struct struct_conversion *conv, va_list list)
 {
-    char nil[] = "(null)";
-    char *new, *s;
-    int len, i, l;
-
-    s = (char *)va_arg(list, char *);
-    if (!s || *s < 0)
-        s = nil;
-
-    len = _strlen(s);
-    for (i = 0; i < len; i++)
-        if ((s[i] > 0 && s[i] < 32) || s[i] >= 127)
-            len += 4;
-
-    new = malloc(sizeof(char) * len);
-    if (!new)
-        exit(1);
-    l = 0;
-    for (i = 0; i < len; i++)
-        if ((s[l] > 0 && s[l] < 32) || s[l] >= 127)
-        {
-            new[i] = '\\', new[i + 1] = 'x';
-            new[i + 2] = (s[l] / 16) >= 10 ? (s[l] / 16 - 10 + 'A') : (s[l] / 16 + '0');
-            new[i + 3] = (s[l] % 16) >= 10 ? (s[l] % 16 - 10 + 'A') : (s[l] % 16 + '0');
-            i += 3;
-            l++;
-        }
-        else
-            new[i] = s[l], l++;
-    len = _putstr(new, len);
-    free(new);
-    return len;
+	char nil[] = "(null)";
+	char *new, *s;
+	int len, i, l;
+	
+	s = (char *)va_arg(list, char *);
+	if (!s || *s < 0)
+		s = nil;
+	
+	len = _strlen(s);
+	l = len;
+	for (i = 0; i < l; i++)
+		if ((s[i] > 0 && s[i] < 32) || s[i] >= 127)
+			len += 3;
+	
+	new = malloc(sizeof(char) * len);
+	if (!new)
+		exit(1);
+	l = 0;
+	for (i = 0; i < len; i++)
+	{
+		if ((s[l] > 0 && s[l] < 32) || s[l] >= 127)
+		{
+			new[i] = '\\', new[i + 1] = 'x';
+			new[i + 2] = (s[l] / 16) >= 10 ? (s[l] / 16 - 10 + 'A') : (s[l] / 16 + '0');
+			new[i + 3] = (s[l] % 16) >= 10 ? (s[l] % 16 - 10 + 'A') : (s[l] % 16 + '0');
+			i += 3;
+			l++;
+		}
+		else
+			new[i] = s[l], l++;
+	}
+	len = _putstr(new, len);
+	free(new);
+	return len;
 }
+
